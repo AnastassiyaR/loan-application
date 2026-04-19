07eeb7aeea473948bb8651b8d92c31379d2c91a4
anrozk <anrozk@taltech.ee>
2026-04-19 19:32:24 +0300

    add missing file

---
+++ backend/src/test/java/com/loanapp/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.loanapp.backend;
+
+
+import com.loanapp.backend.rest.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests extends BaseIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
